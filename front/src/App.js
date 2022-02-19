d41791f8fae7e1dd315f45771242565894a2c0b9
shuufujita <fuchantoshu@gmail.com>
2022-02-19 23:38:51 +0900

    issue-1_001 create react app

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="header">
+        <h1 className="title">Monthly Fun!!!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
