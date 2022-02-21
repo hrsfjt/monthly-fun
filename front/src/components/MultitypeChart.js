b323fd94458782d251fa1af1204e444b5cf9ed3a
shuufujita <fuchantoshu@gmail.com>
2022-02-22 01:49:16 +0900

    issue-3_001 MultitypeChartにJSON経由でデータ投入

--- front/src/components/MultitypeChart.js
+++ front/src/components/MultitypeChart.js
@@ -11,47 +11,14 @@ class MultitypeChart extends React.Component {
         },
         title: {
           display: true,
-          text: 'Chart.js Multitype Chart',
+          text: this.props.title,
         },
       },
     }
 
-    const labels = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-    ]
-
     const data = {
-      labels,
-      datasets: [
-        {
-          type: 'line',
-          label: 'Dataset 1',
-          borderColor: 'rgb(255, 99, 132)',
-          borderWidth: 2,
-          fill: false,
-          data: [10, 200, 120, 80, 40, 50, 60],
-        },
-        {
-          type: 'bar',
-          label: 'Dataset 2',
-          backgroundColor: 'rgb(75, 192, 192)',
-          data: [40, 100, 50, 30, 100, 80, 20],
-          borderColor: 'white',
-          borderWidth: 2,
-        },
-        {
-          type: 'bar',
-          label: 'Dataset 3',
-          backgroundColor: 'rgb(53, 162, 235)',
-          data: [10, 200, 120, 80, 40, 50, 60],
-        },
-      ],
+      labels: this.props.labels,
+      datasets: this.props.datasets,
     }
     return <Chart type="bar" data={data} options={options} />
   }
