44d626488740f4534fa48c56f9abf67832bd20eb
f_h.fujita <h.fujita@fenrir.co.jp>
2022-02-22 12:22:27 +0900

    issue-3_001 customize bar/line chart

--- front/src/components/AppContanier.js
+++ front/src/components/AppContanier.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Sample from '../assets/json/multitype_chart_sample.json'
 import MultitypeChart from './MultitypeChart'
-import LineGraph from './LineGraph'
 
 class AppContanier extends React.Component {
   render() {
     return (
       <div>
-        <LineGraph></LineGraph>
         <MultitypeChart
           title={Sample.title}
           labels={Sample.labels}
