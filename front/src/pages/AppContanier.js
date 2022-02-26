9f1bb928ace791d78136a76981f7203d14a7346c
shuufujita <fuchantoshu@gmail.com>
2022-02-26 15:15:54 +0900

    issue-9_001 ローディング用コンポーネントを実装

--- front/src/pages/AppContanier.js
+++ front/src/pages/AppContanier.js
@@ -2,6 +2,7 @@ import React from 'react'
 import DateFilter from '../components/DateFilter'
 import MultitypeChart from '../components/MultitypeChart'
 import Sample from '../assets/json/multitype_chart_sample.json'
+import Loading from '../components/Loading'
 
 class AppContanier extends React.Component {
   constructor(props) {
@@ -14,6 +15,7 @@ class AppContanier extends React.Component {
       labels: incomeChart.labels,
       datasets: incomeChart.datasets,
       scales: incomeChart.scales,
+      isLoading: false,
     }
     // bind methods
     this.onUpdateFilter = this.onUpdateFilter.bind(this)
@@ -28,15 +30,17 @@ class AppContanier extends React.Component {
   }
 
   render() {
+    const { title, labels, datasets, scales, isLoading } = this.state
     return (
       <div>
         <DateFilter handleUpdate={this.onUpdateFilter}></DateFilter>
         <MultitypeChart
-          title={this.state.title}
-          labels={this.state.labels}
-          datasets={this.state.datasets}
-          scales={this.state.scales}
+          title={title}
+          labels={labels}
+          datasets={datasets}
+          scales={scales}
         ></MultitypeChart>
+        <Loading isLoading={isLoading}></Loading>
       </div>
     )
   }
